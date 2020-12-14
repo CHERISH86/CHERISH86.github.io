726fc8cd02660fb1a95a9869ef3e9bc9ca75416a
CHERISH86 <1033076925@qq.com>
2020-12-14 18:07:57 +0800

    完善红包雨界面功能逻辑，增加抢红包点击事件

--- src/components/EnvelopeRain.vue
+++ src/components/EnvelopeRain.vue
@@ -3,12 +3,25 @@
  * @Autor: 王均祥
  * @Date: 2020-12-09 15:42:29
  * @LastEditors: 王均祥
- * @LastEditTime: 2020-12-14 09:14:21
+ * @LastEditTime: 2020-12-14 17:58:13
 -->
 <template>
-  <div class="">
+  <div>
+  <link rel="stylesheet" href="https://unpkg.com/element-ui/lib/theme-chalk/index.css">
     <div id="redzone">
-      <el-button @click="handleSetEnve()">修改EnvelopeList</el-button>
+    </div>
+    <div>
+      <el-dialog
+        :visible.sync="dialogVisible"
+        width="17%"
+        center
+        @close="handleDialogClose">
+        <img src="../assets/红包(3).png" @click="handleOpenEnve">
+        <span slot="footer" class="dialog-footer">
+          <!-- <el-button @click="centerDialogVisible = false">取 消</el-button> -->
+          <span v-show="moneyVisible">抢到金额：{{envelopeMoney}}</span>
+        </span>
+      </el-dialog>
     </div>
   </div>
 </template>
@@ -19,7 +32,9 @@ export default {
   data () {
     return {
       envelopeList: [1, 2, 3, 1, 5, 6],
-      intervalId: 0
+      envelopeMoney: 0,
+      dialogVisible: false,
+      moneyVisible: false
     }
   },
   computed: {},
@@ -32,7 +47,7 @@ export default {
      * @return {*}
      * @author: 王均祥
      */
-    addBao: function (left, height, src) {
+    addBao: function (left, height, src, id) {
       var div = document.createElement('div')
       var img = document.createElement('img')
       div.appendChild(img)
@@ -42,29 +57,44 @@ export default {
       // 不设置div的高度
       // div.style.height = height + 'px'
       div.className = 'redBox'
-      div.onclick = this.hello
+      div.id = id
+      div.onclick = this.handleEnvelopeClick
       document.getElementById('redzone').appendChild(div)
-      setTimeout(function () {
-        // 滑落到底部时，删除元素
-        // console.log((window.innerHeight - div.getBoundingClientRect().bottom) / window.innerHeight)
-        // if ((window.innerHeight - div.getBoundingClientRect().bottom) / window.innerHeight < 0.1) {
-        // // 避免不是该节点的子元素
-        //   if (document.body.contains(div)) {
-        //     document.getElementById('redzone').removeChild(div)
-        //   }
-        // }
-        // 避免不是该节点的子元素
-        if (document.body.contains(div)) {
-          document.getElementById('redzone').removeChild(div)
-        }
-      }, 4500)
+      console.log('红包ID：' + div.id)
+      // setTimeout(function () {
+      //   // 滑落到底部时，删除元素
+      //   // console.log((window.innerHeight - div.getBoundingClientRect().bottom) / window.innerHeight)
+      //   // if ((window.innerHeight - div.getBoundingClientRect().bottom) / window.innerHeight < 0.1) {
+      //   // // 避免不是该节点的子元素
+      //   //   if (document.body.contains(div)) {
+      //   //     document.getElementById('redzone').removeChild(div)
+      //   //   }
+      //   // }
+      //   // 避免不是该节点的子元素
+      //   if (document.body.contains(div)) {
+      //     console.log('销毁前divid：')
+      //     console.log(div.id)
+      //     document.getElementById('redzone').removeChild(div)
+      //   }
+      // }, 4500)
     },
-    hello () {
-      console.log('hello00000')
+    handleEnvelopeClick () {
+      console.log(event.currentTarget)
+      this.envelopeMoney++
+      this.dialogVisible = true
+      // var oDiv = document.getElementsByClassName('box-out')[0]
+      // 开红包后删除该元素
+      // document.getElementById('redzone').removeChild(event.currentTarget)
     },
-    handleSetEnve () {
-      console.log(this.removeByIndex(this.envelopeList, 1))
-      console.log(this.envelopeList)
+    handleOpenEnve () {
+      // console.log(this.removeByIndex(this.envelopeList, 1))
+      this.moneyVisible = true
+      console.log('抢到了')
+    },
+    handleDialogClose () {
+      this.envelopeMoney = 0
+      this.moneyVisible = false
+      console.log(this.envelopeMoney)
     },
     // 根据数组元素 寻找下标
     indexOfList (list, item) {
@@ -86,19 +116,42 @@ export default {
     }
   },
   mounted () {
+    // axios({
+    //   method: 'post',
+    //   url: '/api/getpageallred',
+    //   data: {
+    //     currentPage: this.pagination.currentPage,
+    //     pageSize: 10
+    //   }
+    // }).then(res => {
+    //   this.tableData = []
+    //   this.tableData = res.data.data.pageRecode
+    //   this.tableData.status = 1
+    // }).catch(function (error) {
+    //   console.log(error)
+    // })
+
     let vueObject = this
-    // 定时器 循环执行 每隔一段时间
-    this.intervalId = setInterval(function () {
+    var divIdList = vueObject.envelopeList
+    // 定时器 循环执行 每隔1500ms
+    var intervalId = setInterval(function () {
       console.log('执行setInterval')
+      // console.log('mounted:' + vueObject.envelopeList)
+      if (divIdList.length === 0) {
+        clearInterval(intervalId)
+        return
+      }
       var left = Math.random() * document.body.scrollWidth
       var height = Math.random() * document.body.scrollHeight
-
+      // 根据后台红包ID设置红包雨divID
+      var id = divIdList.shift()
+      console.log('分配红包ID：' + divIdList.length)
       // assets中的资源，在js中使用，路径要经过webpack中的file-loader编译，故路径不能直接写
       var src = require('../assets/redpacket.png')
       // static下的文件不会被webpack处理，会被直接复制到最终的打包目录下面，所以必须使用绝对路径来引用这些文件
       // var src = '/static/img/redpacket.png'
 
-      vueObject.addBao(left, height, src)
+      vueObject.addBao(left, height, src, id)
     }, 1500)
   }
 }
@@ -117,17 +170,13 @@ export default {
 .redBox {
   position: absolute;
   /* opacity: 0;*/
-  z-index: 10000;
+  /* z-index: 10000; */
   animation: redImgRain 6s infinite linear;
   /* 适配各浏览器 */
   -webkit-animation: redImgRain 6s infinite linear;
   -moz-animation: redImgRain 6s infinite linear;
   -ms-animation: redImgRain 6s infinite linear;
   -o-animation: redImgRain 6s infinite linear;
-}
-
-.redBox {
-  position: fixed;
 }
 .redBox img {
   display: block;
--- src/components/GrabEnvelope.vue
+++ src/components/GrabEnvelope.vue
@@ -1,7 +1,85 @@
+<!--
+ * @Description: 测试开红包动画
+ * @Autor: 王均祥
+ * @Date: 2020-12-08 09:21:37
+ * @LastEditors: 王均祥
+ * @LastEditTime: 2020-12-14 15:40:14
+-->
 <template>
-  <div>
-    <div>
-      <span>快来试试手气吧</span>
-    </div>
-  </div>
+  <div id="hongbao" class="hongbao" style="display:block;" onclick="">
+   <el-dialog
+  title="提示"
+  :visible= true
+  width="30%"
+  center>
+  <span>需要注意的是内容是默认不居中的</span>
+  <span slot="footer" class="dialog-footer">
+    <el-button @click="centerDialogVisible = false">取 消</el-button>
+    <el-button type="primary" @click="centerDialogVisible = false">确 定</el-button>
+  </span>
+</el-dialog>
+ </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    divClick () {
+      console.log(1)
+      document.getElementbyId('open_back').onclick = function (e) {
+        // 屏蔽父层点击事件
+        e.stopPropagation()
+        // 红包开字图片旋转
+        document.getElementbyId('open_back').style.cssText = 'animation: move 3s ease-in-out infinite;'
+        setTimeout(function () {
+          // 结束凯开字图片旋转
+          document.getElementbyId('open_back').style.cssText = 'animation-play-state:paused;'
+        }, 3000)
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.hongbao{
+  background:url('../assets/红包(1).png');
+  /*background:#FFF;*/
+  background-size: 100% 100%;
+  /*filter:progid:DXImageTransform.Microsoft.gradient(startColorstr=#99000000, endColorstr=#99000000);
+  /* For IE 8*/
+  /*-ms-filter: "progid:DXImageTransform.Microsoft.gradient(startColorstr=#99000000, endColorstr=#99000000)";*/
+  width: 75%;
+  height: 17.5rem;
+  /*line-height: 15rem;*/
+  margin-left: 12.5%;
+  margin-top: 8rem;
+  border-radius:0.5rem;
+  /*text-align: center;*/
+}
+.open_div{
+  display:flex;justify-content:center;
+}
+.open_back{
+  background-image:url('../assets/红包(1).png');
+  background-size: 100% 100%;
+  width:5.6rem;
+  height:5.6rem;
+  margin-top: 3rem;
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  /*transform: rotateY(180deg);
+  -webkit-transform: rotateY(180deg);*/
+}
+@keyframes move {
+  0% {
+    transform: rotateY(0deg);
+    -webkit-transform: rotateY(0deg);
+  }
+  100% {
+    transform: rotateY(360deg);
+    -webkit-transform: rotateY(360deg);
+  }
+}
+</style>
